26211e4b33fb1ea75ee31fe514b27ea40f070e92
MdMuzahid07 <mdmuzahid7396@gmail.com>
2024-12-28 19:30:26 +0600

    notification dropdown UI improved

--- src/components/ui/common/navbar/NotificationDropDown.tsx
+++ src/components/ui/common/navbar/NotificationDropDown.tsx
@@ -4,13 +4,13 @@ import NotificationCard from "./NotificationCard";
 
 export default function NotificationDropDown() {
     return (
-        <Dropdown placement="bottom-end" className="w-80 rounded-lg mt-4">
+        <Dropdown placement="bottom-end" className="w-[95vw] sm:w-80 rounded-lg mt-4">
             <DropdownTrigger>
                 <button className="w-7 sm:w-9 h-7 sm:h-9 bg-slate-100 hover:bg-white flex items-center justify-center rounded-full">
                     <Bell className="w-4 sm:w-5 h-4 sm:h-5 text-black" />
                 </button>
             </DropdownTrigger>
-            <DropdownMenu className="max-h-[40vh] overflow-y-auto custom-scrollbar">
+            <DropdownMenu className="max-h-[60vh] sm:max-h-[40vh] overflow-y-auto custom-scrollbar">
                 {
                     [1, 2, 3, 4, 5, 6, 7]?.map((index) => (
                         <DropdownItem key={index} className="p-0"  >
--- src/components/ui/common/navbar/SearchResultCard.tsx
+++ src/components/ui/common/navbar/SearchResultCard.tsx
@@ -51,4 +51,4 @@ export default function SearchResultCard() {
             </CardFooter>
         </Card>
     )
-}
+};
