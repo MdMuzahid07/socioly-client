1d00e246125dc34ac1c4b11dd37dd3c89676d8c0
MdMuzahid07 <mdmuzahid7396@gmail.com>
2025-01-03 19:02:43 +0600

    user active indicatior in chatbox

--- src/components/ui/chat/ChatBoxAside.tsx
+++ src/components/ui/chat/ChatBoxAside.tsx
@@ -25,13 +25,17 @@ export default function ChatBoxAside({ styles }: { styles: string }) {
                             whileHover={{ x: 5 }}
                             transition={{ duration: 0.2 }}
                         >
-                            <div className="relative h-8 w-8 overflow-hidden rounded-full">
+                            <div className="relative h-8 w-8 rounded-full">
                                 <Image
                                     src="https://img.freepik.com/free-vector/gradient-galaxy-background_52683-140963.jpg?t=st=1735897331~exp=1735900931~hmac=c60d4e5d12be4ef4021cd2a082f5bbfaf5970445eff1a4b30260b37b280fdb60&w=1380"
                                     alt={user.name}
                                     fill
-                                    className="object-cover"
+                                    className="object-cover rounded-full"
                                 />
+                                <span className="absolute flex h-2.5 w-2.5 right-0">
+                                    <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-600 opacity-75"></span>
+                                    <span className="relative inline-flex rounded-full h-2.5 w-2.5 bg-green-600"></span>
+                                </span>
                             </div>
                             <div>
                                 <h3 className="font-medium text-sm">{user.name}</h3>
