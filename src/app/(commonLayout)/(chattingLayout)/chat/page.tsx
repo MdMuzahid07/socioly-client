00d07e18e433b801717bd1da6f5a49a7b337bfa2
MdMuzahid07 <mdmuzahid7396@gmail.com>
2025-01-04 09:15:17 +0600

    chatbox inputbar

--- src/app/(commonLayout)/(chattingLayout)/chat/page.tsx
+++ src/app/(commonLayout)/(chattingLayout)/chat/page.tsx
@@ -1,9 +1,16 @@
+import Image from 'next/image'
 import React from 'react'
 
 export default function page() {
     return (
         <div className="bg-slate-100 h-full w-full px-6 flex justify-center items-center">
-
+            <Image
+                src="https://res.cloudinary.com/dlxfcyc7x/image/upload/v1735959790/chat-robot_ggsw95.jpg"
+                alt=""
+                width={250}
+                height={250}
+                className="object-cover rounded-full"
+            />
         </div>
     )
 }
--- src/components/ui/chat/ChatInput.tsx
+++ src/components/ui/chat/ChatInput.tsx
@@ -1,6 +1,41 @@
+"use client";
+import { Button } from "@nextui-org/react";
+import { Plus, Send, Smile } from "lucide-react";
 
 export default function ChatInput({ styles }: { styles: string }) {
     return (
-        <div className={`${styles}`}>ChatInput</div>
+        <div className={`${styles} flex items-center gap-3`}>
+            <div className="flex items-center gap-3">
+                <Button
+                    isIconOnly
+                    className="rounded-full border"
+                    size="sm"
+                >
+                    <Plus className="w-5 h-5" />
+                </Button>
+                <Button
+                    isIconOnly
+                    className="rounded-full"
+                    size="sm"
+                    color="warning"
+                >
+                    <Smile className="w-full h-full text-white" />
+                </Button>
+            </div>
+            <div className="flex items-center w-full gap-3">
+                <input
+                    type="text"
+                    placeholder="Type a message"
+                    className="w-full px-4 py-1 rounded-full border-2 border-gray-200 focus:outline-none focus:border-primary"
+                />
+                <Button
+                    isIconOnly
+                    className="rounded-full bg-blue-700"
+                    size="sm"
+                >
+                    <Send className="w-5 h-5 text-white" />
+                </Button>
+            </div>
+        </div>
     )
-}
+};
